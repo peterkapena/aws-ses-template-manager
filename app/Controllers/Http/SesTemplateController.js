97271104cd69a9c5f892ebca62f0758f518fc583
Kapena Peter Lumumba <peterkapenapeter@gmail.com>
2025-10-07 23:48:44 +0200

    Refactor AWS credentials configuration in SesTemplateController to use environment variables

--- app/Controllers/Http/SesTemplateController.js
+++ app/Controllers/Http/SesTemplateController.js
@@ -1,8 +1,13 @@
 'use strict'
 const Env = use('Env');
 const AWS = require('aws-sdk');
-const credentials = new AWS.SharedIniFileCredentials({profile: Env.get('AWS_PROFILE_NAME', 'default')});
-AWS.config.credentials = credentials;
+
+// Configure AWS credentials from environment variables
+AWS.config.update({
+  accessKeyId: Env.get('AWS_ACCESS_KEY_ID'),
+  secretAccessKey: Env.get('AWS_SECRET_ACCESS_KEY'),
+  region: Env.get('AWS_DEFAULT_REGION', 'us-east-1')
+});
 
 class SesTemplateController {
 
